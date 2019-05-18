a099e7e885be338d3b20773fe4fa349dbd51d672
Steve <steveruizok@gmail.com>
2019-05-18 15:13:35 +0100

    Adds netlify plugin.

--- docz/doczrc.js
+++ docz/doczrc.js
@@ -1,6 +1,7 @@
+import doczPluginNetlify from 'docz-plugin-netlify'
 export default {
-	title: "Framer Controller Docs",
-	description: "Create Control state in Framer X with reusable controllers.",
+	title: 'Framer Controller Docs',
+	description: 'Create Control state in Framer X with reusable controllers.',
 	themeConfig: {},
-	plugins: [],
+	plugins: [doczPluginNetlify()],
 }
